911fa545933030cc1e5c244ccc3061f325e41f41
William Brian Savacool <thesavo@gmail.com>
2013-01-01 18:03:21 -0500

    Update debian-banner/boot-prompt.sh
    Thoughts on using grep

--- debian-banner/boot-prompt.sh
+++ debian-banner/boot-prompt.sh
@@ -4,6 +4,7 @@ _IPADD=`/sbin/ifconfig | sed '/Bcast/!d' | awk '{print $2}' | awk '{print $2}' F
 ### run ipconfig pipe to sed
 ### sed extracts only the line that contains the litteral `Bcast` ie. the Broadcast address of eth`x`
 ### sed !d pulls the line with Bcast and removed leading/Trailing spaces
+### + grep Bcast might also accomplish the same thing
 ### Awk takes out the 2nd block (space delminited) and removes the rest of the line
 ### 2dAwk takes out the 2nd block (: delminited) and removes the rest of the line
 echo "IP Address: $_IPADD" > /etc/issue-addon
