711a75290292809499a4c53d898ed3b80a1b821f
William Brian Savacool <thesavo@gmail.com>
2013-01-01 17:42:51 -0500

    Update debian-banner/boot-prompt.sh
    Add comments to disambiguate command to pickup IP 

--- debian-banner/boot-prompt.sh
+++ debian-banner/boot-prompt.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 IPADD=`/sbin/ifconfig | sed '/Bcast/!d' | awk '{print $2}' | awk '{print $2}' FS=":"`
+### Store in variable
+### run ipconfig pipe to sed
+### 
 echo "IP Address: $IPADD" > /etc/issue-addon
 cat /etc/issue-banner > /etc/issue
 cat /etc/issue-addon >> /etc/issue
